377d9c93b903e66de1d201ae9b51be656a1965e7
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-12-02 14:52:19 -0300

    feat: GenerationModalBody component

---
+++ src/pages/Game/components/GenerationModalBody/GenerationModalBody.tsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+
+//Contexts
+import { GameContetx } from "../../../../contexts/GameContext/GameContext";
+
+//Components
+import CustomCheckbox from "../CustomCheckbox/CustomCheckbox";
+
+//Styles
+import styles from "./styles/GenerationModalBody.styles.module.css";
+
+const GenerationModalBody = () => {
+    const { generationsList } = useContext(GameContetx);
+
+    return (
+        <div className={styles.generations_modal_body}>
+            <h6 className={styles.description}>
+                Selecione gerações para incluir ao jogo.
+            </h6>
+            <div className={styles.checkboxes_container}>
+                {generationsList.map((generation, index) => (
+                    <CustomCheckbox
+                        text={`Geração ${index + 1}`}
+                        checkboxId={generation.name}
+                        defaultChecked={generation.selected}
+                        key={index}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default GenerationModalBody;
