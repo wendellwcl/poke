f2380f2ffac38c8979ee79e19bedbf1c9d435249
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-12-08 13:54:42 -0300

    refactor: improving 'PokemonListContext'

--- src/contexts/PokemonListContext.tsx
+++ src/contexts/PokemonListContext.tsx
@@ -1,3 +1,5 @@
+//This Context is responsible for fetch the complete list with names of all Pokémon
+
 import { createContext, useState, useEffect } from "react";
 
 //Interfaces
@@ -18,10 +20,20 @@ export const PokemonListContext = createContext<IPokemonListContextValue>({
 const PokemonListContextProvider = ({ children }: Props) => {
     const [pokemonList, setPokemonList] = useState<IPokemonShort[]>([]);
 
+    //Fetch list of Pokémon names
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/?limit=10000&offset=0")
-            .then((res) => res.json())
-            .then((res) => setPokemonList(res.results));
+        fetch("https://pokeapi.co/api/v2/pokemoon/?limit=10000&offset=0")
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw "Error fetching Pokémon name list";
+                }
+            })
+            .then((res) => setPokemonList(res.results))
+            .catch((e) => {
+                throw new Error(e);
+            });
     }, []);
 
     const contextValue = {
